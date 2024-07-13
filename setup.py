e78485393ed0fa94f28d3303f13e8210f7d0c4ca
Fatiha-Chaouche <126611907+Fatiha-Chaouche@users.noreply.github.com>
2024-07-13 17:51:59 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Fatyy',
+    author_email= 'kf_chaouche@esi.dz',
+    packages= find_packages(),
+    install_requires = []
+
+)
